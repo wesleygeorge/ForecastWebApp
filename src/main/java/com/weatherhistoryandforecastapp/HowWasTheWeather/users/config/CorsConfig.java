2b28956f040845bd53ba56c11b441fd3d2f88546
Wesley <wesley.t.george@gmail.com>
2025-04-16 21:31:06 +0200

    adding CORS class

---
+++ src/main/java/com/weatherhistoryandforecastapp/HowWasTheWeather/users/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.weatherhistoryandforecastapp.HowWasTheWeather.users.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList(
+                "https://frontend-howwastheweather.onrender.com",
+                "http://localhost:5173" // For local development
+        ));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
